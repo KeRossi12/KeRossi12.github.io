46a4b03cc24ddfcb17065648d48d8b4f3db55918
kpr8402 <kpr8402@rit.edu>
2024-03-28 19:17:50 -0400

    Button Updates

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const divLinks = document.querySelectorAll('.services__card');
+const mainButton = document.querySelector('.main__btn');
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+});
+
+mainButton.addEventListener('click', function() {
+    window.open("/content/Spring 2024 RossiKennethResume.pdf", '_blank').focus();
 });
 
 divLinks.forEach(e => e.addEventListener('click', () => {
